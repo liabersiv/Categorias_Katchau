87b9b8503d3d9ffc78294a43583e6ba12a08edb7
liabersiv <julias140@gmail.com>
2024-10-12 17:47:22 -0300

    Colocando o olho de visualização de senha no input Senha

---
+++ Katchau/assets/SignUp/signUp.js
@@ -0,0 +1,27 @@
+const cadeado = document.getElementById('cadeado');
+const senha = document.getElementById('password');
+const eyesOn = document.getElementById('eyesOn');
+const eyesOff = document.getElementById('eyesOff');
+
+senha.addEventListener('input', () => {
+    if(senha.value.length > 0){
+    cadeado.style.display = 'none';
+    eyesOn.style.display = 'flex';
+    } else { 
+        cadeado.style.display = 'flex';
+        eyesOn.style.display = 'none';
+    }
+})
+
+eyesOn.addEventListener('click', () => {
+    senha.type = 'text';
+    eyesOff.style.display = 'flex';
+    eyesOn.style.display = 'none';
+});
+
+eyesOff.addEventListener('click', () => {
+    senha.type = 'password';
+    eyesOn.style.display = 'flex';
+    eyesOff.style.display = 'none';
+})
+
