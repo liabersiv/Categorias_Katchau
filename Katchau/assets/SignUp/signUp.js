93b28434f56d4113f6040d037f70c22624a7a43e
liabersiv <julias140@gmail.com>
2024-10-12 19:18:40 -0300

    jquery

--- Katchau/assets/SignUp/signUp.js
+++ Katchau/assets/SignUp/signUp.js
@@ -4,15 +4,15 @@ const eyesOn = document.getElementById('eyesOn');
 const eyesOff = document.getElementById('eyesOff');
 
 senha.addEventListener('input', () => {
-    if(senha.value.length > 0){
-    cadeado.style.display = 'none';
-    eyesOn.style.display = 'flex';
-    } else { 
+    if (senha.value.length > 0) {
+        cadeado.style.display = 'none';
+        eyesOn.style.display = 'flex';
+    } else {
         cadeado.style.display = 'flex';
         eyesOn.style.display = 'none';
         eyesOff.style.display = 'none';
     }
-})
+});
 
 eyesOn.addEventListener('click', () => {
     senha.type = 'text';
@@ -24,5 +24,22 @@ eyesOff.addEventListener('click', () => {
     senha.type = 'password';
     eyesOn.style.display = 'flex';
     eyesOff.style.display = 'none';
+});
+
+const cpf = document.getElementById('cpf');
+$(document).ready(function () {
+    $('#cpf').mask('000.000.000-00');
+});
+
+const cep = document.getElementById('cep');
+$(document).ready(function () {
+    $('#cep').mask('00000-000');
+});
+
+const adress = document.getElementById('adress');
+$(document).ready(function () {
+    function rua(value){
+        return 'Rua ' + value;
+    }
 })
 
